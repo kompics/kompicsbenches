7f3408675c1b7ac8e9e5a6398a7333f08bfa82f0
Lars Kroll <lkroll@kth.se>
2019-08-13 18:10:27 +0200

    Finally got erlang commandline args to work

---
+++ erlang/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+_rel/erlang_benchmarks/bin/erlang_benchmarks foreground $@
--- erlang/src/erlang_benchmarks.erl
+++ erlang/src/erlang_benchmarks.erl
@@ -1,11 +1,13 @@
--module(erlang_app).
+-module(erlang_benchmarks).
 -behaviour(application).
 
 -export([start/2]).
 -export([stop/1]).
 
 start(_Type, Args) ->
+	io:fwrite("test~n",[]),
 	erlang:display(Args),
+	erlang:display(application:get_env(erlang_benchmarks, testarg)),
 	erlang_sup:start_link().
 
 stop(_State) ->
