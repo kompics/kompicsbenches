54635923e13d167db1caec83d865fe7c53afe671
Lars Kroll <lkroll@kth.se>
2019-09-05 22:16:36 +0200

    Higher ulimit on OSX (attempt 3)

--- scripts/travis_install_protobuf.sh
+++ scripts/travis_install_protobuf.sh
@@ -6,7 +6,7 @@ NAME="protoc-$VERSION-linux-x86_64.zip"
 URL="https://github.com/protocolbuffers/protobuf/releases/download/v$VERSION/$NAME"
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
-	ulimit -S -n 15000
+	ulimit -n 1024
     # Install protobuf on macOS
     (brew install protobuf@$OSX_VERSION && brew link --force --overwrite protobuf@$OSX_VERSION) || brew upgrade protobuf
 else
