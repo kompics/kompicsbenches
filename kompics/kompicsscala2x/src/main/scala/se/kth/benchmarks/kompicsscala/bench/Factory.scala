a007463728a583743ff146bcca613cbfe913983f
haraldng <hng@kth.se>
2020-05-04 11:28:11 +0200

    Added NotImplemented in other frameworks and some cleanup for test on Selene

--- kompics/kompicsscala2x/src/main/scala/se/kth/benchmarks/kompicsscala/bench/Factory.scala
+++ kompics/kompicsscala2x/src/main/scala/se/kth/benchmarks/kompicsscala/bench/Factory.scala
@@ -12,4 +12,5 @@ object Factory extends BenchmarkFactory {
   override def fibonacci: Benchmark = Fibonacci;
   override def chameneos(): Benchmark = Chameneos;
   override def allPairsShortestPath(): Benchmark = AllPairsShortestPath;
+  override def atomicBroadcast(): DistributedBenchmark = ???;
 }
