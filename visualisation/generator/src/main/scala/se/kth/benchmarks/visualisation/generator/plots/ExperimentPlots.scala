b9d94b8f4964e8e3cf71cbe7f485bc3ac2a4180b
haraldng <hng@kth.se>
2020-05-10 17:35:19 +0200

    Plotting forward_discarded and other cleanup

--- visualisation/generator/src/main/scala/se/kth/benchmarks/visualisation/generator/plots/ExperimentPlots.scala
+++ visualisation/generator/src/main/scala/se/kth/benchmarks/visualisation/generator/plots/ExperimentPlots.scala
@@ -10,8 +10,8 @@ import scala.collection.immutable
 abstract class ExperimentPlots[Params <: Parameters.Message[Params]](val bench: BenchmarkWithSpace[Params],
                                                                      val space: ParameterSpacePB[Params],
                                                                      val data: BenchmarkData[Params]) {
-  def plotAlong[T: Ordering: ClassTag, G: Ordering, S](mainAxis: Params => T, // Ex: num_keys
-                                                       groupings: Params => G,  // ex read-write-partition_size
+  def plotAlong[T: Ordering: ClassTag, G: Ordering, S](mainAxis: Params => T,
+                                                       groupings: Params => G,
                                                        plotId: G => String,
                                                        plotParams: G => List[String],
                                                        plotTitle: String,
@@ -26,34 +26,21 @@ abstract class ExperimentPlots[Params <: Parameters.Message[Params]](val bench: 
                                                        calculatedYAxisLabel: String,
                                                        calculatedUnits: String): PlotGroup.Along = {
     val sliced = data.slices(groupings, mainAxis);
-    println(s"\nsliced: $sliced");
     val plots: List[PlotGroup] = sliced.toList.map {
-      case (params, impls) => { // Ex: (r-50-w-50, partiion_size = 3, kompact) TODO REMOVE
-        println("\nparams: " + params + ", impls: " + impls);
+      case (params, impls) => {
         val paramSeries = data.paramSeries(mainAxis);
-        println("\nparamSeries: " + paramSeries.mkString(", "));
         val groupedSeries = impls.mapValues(_.map2D(identity, paramSeries));
         val groupedErrorSeries = impls.mapValues(_.map2DErrorBars(identity, paramSeries));
-        groupedSeries.foreach {
-          case (key, value) => {
-            println(s"\nGROUPED_SERIES key: $key, value-data: ${value.data.mkString(", ")}")
-          }
-        }
-//        println("\ngroupedseries: " + groupedSeries);
         val mergedSeries = (for (key <- groupedSeries.keys) yield {
           (key, groupedSeries(key), groupedErrorSeries(key))
         }).toList;
-        println("\nmerged series: " + mergedSeries);
         val sortedSeries: List[Series] = mergedSeries.sortBy(_._1).map(t => List[Series](t._2, t._3)).flatten;
-        println("\nsorted series: " + sortedSeries);
-
         val pimpedSeries: List[Series] = sortedSeries.map(
           _.addMeta(
             "tooltip" -> JsRaw(s"{valueDecimals: 2, valueSuffix: '${units}'}")
           )
         );
         val paramsS = paramSeries.map(_.toString);
-        paramsS.foreach(x => println("paramsS: " + x));
         val corePlotId = s"${bench.symbol.toLowerCase()}-${plotId(params)}";
         val primaryPlotId = s"${corePlotId}-primary";
         val primaryPlot = PlotData(
@@ -135,8 +122,8 @@ abstract class ExperimentPlots[Params <: Parameters.Message[Params]](val bench: 
 
   }
 
-  def plotAlongPreSliced[T: Ordering: ClassTag, G: Ordering, S](mainAxis: Params => T, // Ex: num_keys
-                                                       groupings: Params => G,  // ex read-write-partition_size
+  def plotAlongPreSliced[T: Ordering: ClassTag, K: Ordering, G: Ordering, S](mainAxis: Params => T,
+                                                       groupings: Params => G,
                                                        plotId: G => String,
                                                        plotParams: G => List[String],
                                                        plotTitle: String,
@@ -150,36 +137,24 @@ abstract class ExperimentPlots[Params <: Parameters.Message[Params]](val bench: 
                                                        calculatedTitle: String,
                                                        calculatedYAxisLabel: String,
                                                        calculatedUnits: String,
-                                                       sliced: scala.collection.SortedMap[G, immutable.SortedMap[String, ImplGroupedResult[T]]]
+                                                       seriesParams: Params => K,
+                                                       sliced: scala.collection.SortedMap[G, immutable.SortedMap[K, ImplGroupedResult[T]]]
                                                        ): PlotGroup.Along = {
-//    val sliced = data.slices(groupings, mainAxis);
     val plots: List[PlotGroup] = sliced.toList.map {
-      case (params, impls) => { // Ex: (r-50-w-50, partiion_size = 3, kompact) TODO REMOVE
-        println("\nparams: " + params + ", impls: " + impls);
+      case (params, impls) => {
         val paramSeries = data.paramSeries(mainAxis);
-        println("\nparamSeries: " + paramSeries.mkString(", "));
         val groupedSeries = impls.mapValues(_.map2D(identity, paramSeries));
         val groupedErrorSeries = impls.mapValues(_.map2DErrorBars(identity, paramSeries));
-        groupedSeries.foreach {
-          case (key, value) => {
-            println(s"\nGROUPED_SERIES key: $key, value-data: ${value.data.mkString(", ")}")
-          }
-        }
-        //        println("\ngroupedseries: " + groupedSeries);
         val mergedSeries = (for (key <- groupedSeries.keys) yield {
           (key, groupedSeries(key), groupedErrorSeries(key))
         }).toList;
-        println("\nmerged series: " + mergedSeries);
-        val sortedSeries: List[Series] = mergedSeries.sortBy(_._1).map(t => List[Series](t._2, t._3)).flatten;
-        println("\nsorted series: " + sortedSeries);
-
+        val sortedSeries: List[Series] = mergedSeries.map(t => List[Series](t._2, t._3)).flatten; // already sorted preslice
         val pimpedSeries: List[Series] = sortedSeries.map(
           _.addMeta(
             "tooltip" -> JsRaw(s"{valueDecimals: 2, valueSuffix: '${units}'}")
           )
         );
         val paramsS = paramSeries.map(_.toString);
-        paramsS.foreach(x => println("paramsS: " + x));
         val corePlotId = s"${bench.symbol.toLowerCase()}-${plotId(params)}";
         val primaryPlotId = s"${corePlotId}-primary";
         val primaryPlot = PlotData(
