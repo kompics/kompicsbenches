df1033a33698204c2f791709f48f6c5c6f4ec821
haraldng <hng@kth.se>
2020-08-16 19:53:26 +0200

    Quick start flag in BLE to prevent timeouts due to too fast new followers. Static BATCH_ACCEPT flag.

--- kompact/src/bench/atomic_broadcast/parameters.rs
+++ kompact/src/bench/atomic_broadcast/parameters.rs
@@ -2,15 +2,17 @@ pub const ELECTION_TIMEOUT: u64 = 5000;
 pub const OUTGOING_MSGS_PERIOD: u64 = 1;
 pub const MAX_INFLIGHT: usize = 10000000;   // capacity of number of messages in parallel. Set to max batch size in experiment test space
 pub const INITIAL_ELECTION_FACTOR: u64 = 10;   // shorter first election: ELECTION_TIMEOUT/INITIAL_ELECTION_FACTOR
-pub const META_RESULTS_DIR: &str = "../meta_results/meta-arcon1"; // change for each benchmark
+pub const META_RESULTS_DIR: &str = "../meta_results/meta-arcon1-reconfiguration"; // change for each benchmark
 
 pub mod paxos {
     pub const GET_DECIDED_PERIOD: u64 = 1;
     pub const TRANSFER_TIMEOUT: u64 = 300;
     pub const BLE_DELTA: u64 = 100;
+    pub const BATCH_ACCEPT: bool = true;
     pub const BATCH_DECIDE: bool = true;
+    pub const MAX_ACCSYNC: bool = false;
     pub const BLE_PRIO_START: bool = true;
-    pub const MAX_ACCSYNC: bool = true;
+    pub const PRIO_START_ROUND: u64 = 10;
 }
 
 pub mod raft {
