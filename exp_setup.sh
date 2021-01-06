64d937f6829d78eb24a25dc21f79ec8d66bb77cf
haraldng <hng@kth.se>
2021-01-06 19:29:24 +0100

    Only master automatically generate experimental setup data. Added support for command line usage

--- exp_setup.sh
+++ exp_setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-file=$1/experimental_setup.out
-touch $file
-echo "System: $(eval "uname -a")" >> $file
-echo "Git: branch: $(eval "git rev-parse --abbrev-ref HEAD"), hash: $(eval "git rev-parse HEAD")" >> $file
-echo "Rust: $(eval "rustc --version")" >> $file
-echo "Java: $(eval "java --version")" >> $file
-echo "Erlang: $(eval "erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), \"releases\", erlang:system_info(otp_release), \"OTP_VERSION\"])), io:fwrite(Version), halt().' -noshell")" >> $file
+declare -a s=(
+	"System: $(uname -a)" 
+	"Git: branch: $(git rev-parse --abbrev-ref HEAD), commit: $(git rev-parse HEAD)"
+	"Rust: $(rustc --version)"
+	"Java: $(java --version)"
+	"Erlang: $(erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' -noshell)"
+)
+if [ -z "$1" ]
+  then
+    for val in "${s[@]}"; do
+  		echo $val
+	done
+else
+	file=$1/experimental_setup.out
+	touch $file
+    for val in "${s[@]}"; do
+  		echo $val >> $file
+	done
+fi
