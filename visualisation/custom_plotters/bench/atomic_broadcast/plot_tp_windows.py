c31594bc26b08c18d3597c718911abd97ac8a9d0
haraldng <hng@kth.se>
2021-04-24 12:54:45 +0200

    Min, Max, Avg per window

--- visualisation/custom_plotters/bench/atomic_broadcast/plot_tp_windows.py
+++ visualisation/custom_plotters/bench/atomic_broadcast/plot_tp_windows.py
@@ -6,6 +6,16 @@ import sys
 import argparse
 import os
 import numpy as np
+import datetime
+
+def format_time(seconds):
+    """Formats a timedelta duration to [N days] %M:%S format"""
+    secs_in_a_min = 60
+
+    minutes, seconds = divmod(seconds, secs_in_a_min)
+
+    time_fmt = "{:d}:{:02d}".format(minutes, seconds)
+    return time_fmt
 
 parser = argparse.ArgumentParser()
 
@@ -17,20 +27,39 @@ args = parser.parse_args()
 print("Plotting with args:",args)
 
 f = open(args.s, 'r')
-all_ts = []
 all_tp = []
 for line in f:
 	ts_throughput = line.split(" ")
 	start_ts = int(ts_throughput[0].split(",")[0])
-	for csv in ts_throughput:
+	for window_idx, csv in enumerate(ts_throughput):
 		ts_tp = csv.split(",")
-		ts = (int(ts_tp[0]) - start_ts) / 1000000000	# convert from ns to s
+		#ts = (int(ts_tp[0]) - start_ts) / 1000000000	# convert from ns to s
 		tp = int(ts_tp[1]) / args.w
-		all_ts.append(ts)
-		all_tp.append(tp)
+		#all_ts.append(ts)
+		if len(all_tp) <= window_idx:
+			all_tp.append([])
+		all_tp[window_idx].append(tp)
 
-plt.plot(all_ts, all_tp, marker='o')
-plt.xticks(np.arange(min(all_ts), max(all_ts) + args.w, args.w))
+all_ts = []
+all_avg_tp = []
+all_min_tp = []
+all_max_tp = []
+for i in range(len(all_tp)):
+	all_ts.append(format_time(seconds=(i+1) * args.w))
+	avg_tp = sum(all_tp[i])/len(all_tp[i])
+	min_tp = min(all_tp[i])
+	max_tp = max(all_tp[i])
+
+	all_avg_tp.append(avg_tp)
+	all_min_tp.append(min_tp)
+	all_max_tp.append(max_tp)
+
+
+plt.plot(all_ts, all_avg_tp, marker='o')
+plt.plot(all_ts, all_min_tp, marker='o')
+plt.plot(all_ts, all_max_tp, marker='o')
+plt.gcf().autofmt_xdate()
+#plt.xticks(0, len(all_avg_tp) + args.w, args.w)
 if args.t is not None:
     target_dir = args.t
 else:
