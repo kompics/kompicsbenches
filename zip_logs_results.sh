72b56e7f33e0f8c851a2a44a72cd46e04e51168c
Harald Ng <hng@kth.se>
2021-05-01 09:13:20 +0200

    Compress zip files

--- zip_logs_results.sh
+++ zip_logs_results.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p zipped
-zip -r zipped/$1 results/$1/ logs/$1/ remote_logs/$1/ meta_results/$1/ -qi '*.data' '*.out' '*.error' 
+zip -r zipped/$1 results/$1/ logs/$1/ remote_logs/$1/ meta_results/$1/ -qi '*.data' '*.out' '*.error' '*.zip'
 echo "Results, logs and meta results of $1 compressed into zipped/$1.zip"
