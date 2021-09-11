9a0f776675e973043b0de639af356b91183e273a
Harald Ng <hng@kth.se>
2021-09-11 12:30:52 +0200

    Chained and Deadlock experiments

--- runner/src/main/scala/se/kth/benchmarks/runner/Benchmarks.scala
+++ runner/src/main/scala/se/kth/benchmarks/runner/Benchmarks.scala
@@ -239,11 +239,11 @@ object Benchmarks extends ParameterDescriptionImplicits {
   private val atomicBroadcastTestProposals = List(1L.k);
   private val atomicBroadcastTestConcurrentProposals = List(200L);
 
-  private val atomicBroadcastNodes = List(3);
-  private val atomicBroadcastProposals = List(10L.mio);
+  private val atomicBroadcastNodes = List(3, 5);
+  private val atomicBroadcastProposals = List(5L.mio);
   private val atomicBroadcastConcurrentProposals = List(500L);
 
-  private val algorithms = List("raft");
+  private val algorithms = List("raft", "paxos");
   private val reconfig = List("single", "majority");
   private val reconfig_policy = List("replace-follower", "replace-leader");
 
