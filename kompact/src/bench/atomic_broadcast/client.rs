7922434a1503af9d0fe8ed298280cdb70c909538
Harald Ng <hng@kth.se>
2021-09-12 18:57:09 +0200

    Update client.rs

--- kompact/src/bench/atomic_broadcast/client.rs
+++ kompact/src/bench/atomic_broadcast/client.rs
@@ -256,8 +256,10 @@ impl Client {
                 if let Some(timer) = self.window_timer.take() {
                     self.cancel_timer(timer);
                 }
-                if let Some(timer) = self.periodic_partition_timer.take() {
-                    self.cancel_timer(timer);
+                #[cfg(feature = "simulate_partition")] {
+                    if let Some(timer) = self.periodic_partition_timer.take() {
+                        self.cancel_timer(timer);
+                    }
                 }
                 self.state = ExperimentState::Finished;
                 self.finished_latch
