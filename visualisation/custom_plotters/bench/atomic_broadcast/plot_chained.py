38ace7073ba879f34866df128c40c38e4efaae8b
Harald Ng <harald.ng@hotmail.com>
2021-12-28 18:00:24 +0100

    Update plot_chained.py

--- visualisation/custom_plotters/bench/atomic_broadcast/plot_chained.py
+++ visualisation/custom_plotters/bench/atomic_broadcast/plot_chained.py
@@ -8,9 +8,9 @@ def ms_to_tp(t):
 
 FILENAME = "chained"
 TITLE = 'Chained scenario'
-raft = [72992.598572, 49096.596413, 52026.085097999996, 57850.760213999994, 48789.793243, 52498.125788, 49160.668132, 72751.197831, 61090.073779, 51998.231606999994]
-raft_pv = [43329.604495,43388.161511,43767.704653,42241.795067,42119.735282,43769.912786,44565.963586,43569.776734,43557.760668999996,42316.443001]
-paxos = [50655.495052, 50384.418675, 49658.661265999996, 50432.528299, 49910.884253,50599.022819, 51244.531276999995, 50479.078968999995, 50542.043344, 51361.560896999996]
+raft = [50533.435782,52911.101637,60091.845166,65547.680951,54038.709471999995,74749.261094,90319.950333,60373.700495,60754.824064,69841.72998399999]
+raft_pv = [50377.20796,47050.350992,46920.739193999994,45494.902544,46634.340917999994,46939.709022999996,51820.560351,50758.268513999996,51059.424573,48731.035656]
+paxos = [50505.792305999996,50304.649215,50369.734166999995,49491.659029999995,49579.429575999995,49768.561979,49150.848915999995,49201.637051,49719.578301,49750.827285]
 
 raft_s = list(map(ms_to_tp, raft))
 raft_pv_s = list(map(ms_to_tp, raft_pv))
@@ -18,14 +18,13 @@ paxos_s = list(map(ms_to_tp, paxos))
 
 my_dict = {'Raft': raft_s, 'Omni-Paxos': paxos_s, 'Raft PV+CQ': raft_pv_s}
 
-#y_axis = np.arange(40, 75, 5)
+y_axis = np.arange(0, 130000, 20000)
 
 MEDIUM_SIZE = 18
 SIZE = 20
 plt.rc('axes', labelsize=SIZE) 
 plt.rc('xtick', labelsize=SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SIZE)    # fontsize of the tick labels
-#plt.yticks(y_axis)
 
 fig, ax = plt.subplots()
 #fig.set_size_inches(10.3, 6)
@@ -35,7 +34,7 @@ ax.boxplot(my_dict.values())
 #ax.boxplot(my_dict.values(), positions=[1, 1.6, 2.2])
 ax.set_xticklabels(my_dict.keys())
 #ax.set_ylabel('Throughput (ops/s)')
-#ax.set_yticks(y_axis)
+ax.set_yticks(y_axis)
 ax.yaxis.set_major_formatter(util.format_k)
 
 plt.savefig("{}.pdf".format(FILENAME), dpi = 600, bbox_inches='tight')
